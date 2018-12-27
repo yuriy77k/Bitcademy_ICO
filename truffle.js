5dcd28bc4f2cae6c4705ad3f02541b4c829c954b
Kelvin Ndereba <nderebakelvin@gmail.com>
2018-12-27 20:15:29 +0300

    remove seed
    Remove seed

--- truffle.js
+++ truffle.js
@@ -1,7 +1,6 @@
 //const HDWalletProvider = require("truffle-hdwallet-provider-privkey");
 const privKey = "<private key here>";
-//const mnemonic = "come hover maple exhaust proud invite sweet elegant seven pair stock toy"; 
-const mnemonic = "clump diagram small enter enroll bench half ribbon appear useless monitor route";
+const mnemonic = "";
 const HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
